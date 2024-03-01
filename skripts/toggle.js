e08a59e24a352275648e0dca03a7ff4f53bdc8af
alexeeey_03 <burchakxlx@gmail.com>
2024-03-02 00:40:13 +0300

    v 1.0

---
+++ skripts/toggle.js
@@ -0,0 +1,15 @@
+function toggleContainer(id, arrow) {
+    var container = document.getElementById(id);
+    var arrowDown = arrow.querySelector('.arrow-down');
+    var arrowUp = arrow.querySelector('.arrow-up');
+
+    if (container.style.display === "none" || container.style.display === "") {
+        container.style.display = "block";
+        arrowDown.style.display = "none";
+        arrowUp.style.display = "inline-block";
+    } else {
+        container.style.display = "none";
+        arrowDown.style.display = "inline-block";
+        arrowUp.style.display = "none";
+    }
+}
