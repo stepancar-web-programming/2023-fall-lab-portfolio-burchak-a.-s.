02b8df4daa7604559fed5defb896ff41bc9352ac
alexeeey03 <070903Burchak>
2024-03-07 00:46:16 +0300

    formated code

---
+++ scripts/slide.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+  },
+});
