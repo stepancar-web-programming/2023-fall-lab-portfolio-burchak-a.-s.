9a273efb28669324347aac317e19e7e2cbee3d68
alexeeey03 <070903Burchak>
2024-03-07 01:23:14 +0300

    fix linter errors in js

--- scripts/slide.js
+++ scripts/slide.js
@@ -1,8 +1,8 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper('.mySwiper', {
   slidesPerView: 1,
   spaceBetween: 30,
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
     clickable: true,
   },
   breakpoints: {
